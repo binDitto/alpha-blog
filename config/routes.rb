a50e9f7b5d1b0bac3b5d29c532fe3344666892a6
Bin udemy Rails <sudocode.bin@gmail.com>
2016-11-08 08:46:52 +0000

    Signup users from UI

--- config/routes.rb
+++ config/routes.rb
@@ -9,7 +9,8 @@ Rails.application.routes.draw do
   
   resources :articles
   
-  
+  get 'signup', to: 'users#new'
+  post 'users', to: 'users#create'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
