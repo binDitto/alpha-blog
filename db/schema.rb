6f5f2b73c9bbb3837d37f8ee5ceac9f41101b2ff
Bin udemy Rails <sudocode.bin@gmail.com>
2016-11-12 10:12:47 +0000

    Add article and category association

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161111065519) do
+ActiveRecord::Schema.define(version: 20161112094540) do
+
+  create_table "artcile_categores", force: :cascade do |t|
+    t.integer "article_id"
+    t.integer "category_id"
+  end
+
+  create_table "article_categories", force: :cascade do |t|
+    t.integer "article_id"
+    t.integer "category_id"
+  end
 
   create_table "articles", force: :cascade do |t|
     t.string   "title"
